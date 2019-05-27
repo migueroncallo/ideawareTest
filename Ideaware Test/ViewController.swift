3d0a42a975a78fb370b2058ca7dc155db7afc186
Miguel Roncallo <miguel@zipdev.co>
2019-05-27 15:25:57 -0500

    Initial Commit

---
+++ Ideaware Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ideaware Test
+//
+//  Created by Miguel Roncallo on 5/27/19.
+//  Copyright © 2019 Miguel Roncallo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
